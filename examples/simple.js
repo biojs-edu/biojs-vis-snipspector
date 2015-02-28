f21dba29f4be08e1397b6bb224a7d96d6c371668
Sebastian Wilzbach <seb@wilzba.ch>
2015-02-28 09:47:55 +0100

    made url configurable

--- examples/simple.js
+++ examples/simple.js
@@ -1,3 +1,3 @@
 // if you don't specify a html file, the sniper will generate a div
 app = require("biojs-vis-snipspector");
-app({el: yourDiv});
+app({el: yourDiv, url: "http://files.biojs.net/chromosomes/manny"});
